b9bdabd144f13d186d49c6e022d64a5848f06f36
Jose Aguilar <jaguilarny@gmail.com>
2021-03-01 12:41:48 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 function addingEventListener() {
-}
+    const input = document.getElementById('input');
+    const div = document.querySelectorAll('div')[5];
+    input.addEventListener('click', clickAlert);
+    div.addEventListener('click', clickAlert);
+    
+};
+
+addingEventListener();
+
+function clickAlert() {
+    alert('I was clicked!');
+};
